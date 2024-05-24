08119d9ec3d446c528eb5744b1a9b669f132ba1f
glennlum <glennlum_@hotmail.com>
2024-05-24 17:05:04 +0800

    feat(my_functions): my_functions added

---
+++ source/my_functions.py
@@ -0,0 +1,10 @@
+def add(number_one, number_two):
+    if not (isinstance(number_one, int) and isinstance(number_two, int)):
+        raise TypeError
+    return number_one + number_two
+
+
+def divide(number_one, number_two):
+    if number_two == 0:
+        raise ValueError
+    return number_one / number_two
