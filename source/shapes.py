a73bd4157cc3198824f3ac73b1cf00531fff58cf
glennlum <glennlum_@hotmail.com>
2024-05-24 18:07:29 +0800

    feat(shapes): add shape classes

---
+++ source/shapes.py
@@ -0,0 +1,23 @@
+import math
+from abc import ABCMeta
+
+
+class Shape(metaclass=ABCMeta):
+
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
