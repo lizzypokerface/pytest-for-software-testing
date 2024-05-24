09fa8ea695cdc9b9f551cbf6a03666a0a204bbc2
glennlum <glennlum_@hotmail.com>
2024-05-25 00:00:36 +0800

    feat(my_functions): add functions

--- source/my_functions.py
+++ source/my_functions.py
@@ -8,3 +8,7 @@ def divide(number_one, number_two):
     if number_two == 0:
         raise ValueError
     return number_one / number_two
+
+
+def multiply(number_one, number_two):
+    return number_one ** number_two
