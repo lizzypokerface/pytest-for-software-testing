fda1a3f3e0a9bae46403357b8c39d3bb73d0b4a6
glennlum <glennlum_@hotmail.com>
2024-05-25 21:05:39 +0800

    style(service): tidy comment

--- tests/test_service.py
+++ tests/test_service.py
@@ -7,7 +7,6 @@ import source.service as service
 
 
 # https://docs.python.org/3/library/unittest.mock.html
-# Use mocking when tests are dependednt on external programs that cannot guarantee the same result each time
 
 @mock.patch("source.service.get_user_from_db")
 def test_get_user_from_db(mock_get_user_from_db):
